9770d1da464617656d5f1870dc50406052d81e95
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2018-04-20 20:35:43 +0900

    add hash api
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>

--- cache.h
+++ cache.h
@@ -19,6 +19,7 @@
 #ifndef __CIFSD_CACHE_H__
 #define __CIFSD_CACHE_H__
 
+#include <linux/hashtable.h>
 #include <linux/rwsem.h>
 #include <linux/radix-tree.h>
 #include <linux/atomic.h>
@@ -28,40 +29,9 @@ struct cifsd_cache {
 	struct radix_tree_root	rt;
 	atomic_long_t		next_id;
 
-	void *(*lookup_fn)(void *val);
-	void (*destructor_fn)(void *val);
+	void 			*(*lookup_fn)(void *val);
+	void 			(*destructor_fn)(void *val);
 };
-
-static void cifsd_cache_destroy(struct cifsd_cache *cache)
-{
-	struct radix_tree_iter iter;
-	void **slot;
-
-	down_write(&cache->lock);
-	radix_tree_for_each_slot(slot, &cache->rt, &iter, 0) {
-		void *val = radix_tree_deref_slot(slot);
-
-		if (val && cache->destructor_fn) {
-			slot = radix_tree_iter_resume(slot, &iter);
-			up_write(&cache->lock);
-			cache->destructor_fn(val);
-			down_write(&cache->lock);
-		}
-	}
-	up_write(&cache->lock);
-}
-
-static void cifsd_cache_init(struct cifsd_cache *cache,
-			     void *(*lookup_fn)(void *val),
-			     void (*destructor_fn)(void *val))
-{
-	INIT_RADIX_TREE(&cache->rt, GFP_KERNEL);
-
-	init_rwsem(&cache->lock);
-	atomic_long_set(&cache->next_id, 0);
-	cache->lookup_fn = lookup_fn;
-	cache->destructor_fn = destructor_fn;
-}
 
 static void *cifsd_cache_lookup(struct cifsd_cache *cache, unsigned long id)
 {
@@ -117,5 +87,132 @@ static int cifsd_cache_remove(struct cifsd_cache *cache, unsigned long key)
 	radix_tree_delete(&cache->rt, key);
 	up_write(&cache->lock);
 	return 0;
+}
+
+static void cifsd_cache_destroy(struct cifsd_cache *cache)
+{
+	struct radix_tree_iter iter;
+	void **slot;
+
+	down_write(&cache->lock);
+	radix_tree_for_each_slot(slot, &cache->rt, &iter, 0) {
+		void *val = radix_tree_deref_slot(slot);
+
+		if (val && cache->destructor_fn) {
+			slot = radix_tree_iter_resume(slot, &iter);
+			up_write(&cache->lock);
+			cache->destructor_fn(val);
+			down_write(&cache->lock);
+		}
+	}
+	up_write(&cache->lock);
+}
+
+static int cifsd_cache_init(struct cifsd_cache *cache,
+			     void *(*lookup_fn)(void *val),
+			     void (*destructor_fn)(void *val))
+{
+	INIT_RADIX_TREE(&cache->rt, GFP_KERNEL);
+
+	init_rwsem(&cache->lock);
+	atomic_long_set(&cache->next_id, 0);
+	cache->lookup_fn = lookup_fn;
+	cache->destructor_fn = destructor_fn;
+	return 0;
+}
+
+/***********************************************************************/
+
+struct cifsd_hash {
+	struct rw_semaphore	lock;
+	struct hlist_head	*hash;
+	size_t			size_bits;
+
+	int			(*lookup_fn)(struct hlist_node *node,
+					     unsigned long id);
+	void 			(*destructor_fn)(struct hlist_node *t);
+};
+
+static struct hlist_node*
+cifsd_hash_lookup(struct cifsd_hash *ht, unsigned long key)
+{
+	int cmp = -EINVAL;
+	unsigned long k;
+	struct hlist_head *hhd;
+	struct hlist_node *node;
+
+	k = hash_long(key, ht->size_bits);
+	hhd = &ht->hash[k];
+
+	down_read(&ht->lock);
+	hlist_for_each(node, hhd) {
+		if (ht->lookup_fn)
+			cmp = ht->lookup_fn(node, key);
+		if (cmp == 0)
+			break;
+	}
+	node = NULL;
+	up_read(&ht->lock);
+	return node;
+}
+
+static int cifsd_hash_insert(struct cifsd_hash *ht,
+			     unsigned long key,
+			     struct hlist_node *node)
+{
+	unsigned long k = hash_long(key, ht->size_bits);
+
+	down_write(&ht->lock);
+	hlist_add_head(node, &ht->hash[k]);
+	up_write(&ht->lock);
+	return 0;
+}
+
+static int cifsd_hash_remove(struct cifsd_hash *ht,
+			     struct hlist_node *node)
+{
+	down_write(&ht->lock);
+	hlist_del(node);
+	up_write(&ht->lock);
+	return 0;
+}
+
+static void cifsd_hash_destroy(struct cifsd_hash *ht)
+{
+	struct hlist_head *hhd;
+	int i;
+	int size = 1 << ht->size_bits;
+
+	down_write(&ht->lock);
+	for (i = 0; i < size; i++) {
+		hhd = &ht->hash[i];
+
+		while (!hlist_empty(hhd)) {
+			if (ht->destructor_fn) {
+				up_write(&ht->lock);
+				ht->destructor_fn(hhd->first);
+				down_write(&ht->lock);
+			}
+		}
+	}
+	up_write(&ht->lock);
+}
+
+static int
+cifsd_hash_init(struct cifsd_hash *ht,
+		size_t size_bits,
+		int (*lookup_fn)(struct hlist_node *node, unsigned long id),
+		void (*destructor_fn)(struct hlist_node *t))
+{
+	size_t sz = 1 << size_bits;
+
+	ht->hash = kzalloc(sizeof(struct hlist_head) * sz, GFP_KERNEL);
+	if (!ht->hash)
+		return -ENOMEM;
+
+	ht->size_bits = size_bits;
+	init_rwsem(&ht->lock);
+	ht->lookup_fn = lookup_fn;
+	ht->destructor_fn = destructor_fn;
 }
 #endif /* __CIFSD_CACHE_H__ */
